aa001625d91ed6da3beef5452ec20b22d5e7b230
sm <smlee@danal.co.kr>
2024-03-11 17:45:15 +0900

    version 2

--- src/main/java/com/example/dataflow/JdbcSpringCloudSink/DebeziumData.java
+++ src/main/java/com/example/dataflow/JdbcSpringCloudSink/DebeziumData.java
@@ -26,12 +26,11 @@ public record DebeziumData(
                 afterTemp = (HashMap<String, Object>) Optional.ofNullable(map.get("after")).orElseThrow(() -> new NoSuchElementException("AFTER"));
             if (beforeArr.contains(opTemp))
                 beforeTemp = (HashMap<String, Object>) Optional.ofNullable(map.get("before")).orElseThrow(() -> new NoSuchElementException("BEFORE"));
-            sourceTemp = (HashMap<String, Object>) Optional.of(map.get("source")).orElseThrow();
+            sourceTemp = (HashMap<String, Object>) Optional.ofNullable(map.get("source")).orElseThrow(() -> new NoSuchElementException("SOURCE"));
             ts_msTemp = Optional.ofNullable(map.get("ts_ms")).orElse(null).toString();
             transactionTemp = (String) Optional.ofNullable(map.get("transaction")).orElse(null);
         } catch (NoSuchElementException ignored) {
             log.error("data not match : {}", ignored.getMessage());
-            log.error(ignored.getCause().getMessage());
             throw new DebeziumDataNotMatchException(map);
         }
         return new DebeziumData(beforeTemp, afterTemp, sourceTemp, opTemp, ts_msTemp, transactionTemp);
--- src/test/java/com/example/dataflow/JdbcSpringCloudSink/ProcessServiceTest.java
+++ src/test/java/com/example/dataflow/JdbcSpringCloudSink/ProcessServiceTest.java
@@ -24,7 +24,7 @@ class ProcessServiceTest {
     @Test
     @Transactional
     void insertTest() throws JsonProcessingException {
-        String message = "{\"payload\":{\"before\":null,\"after\":{\"seq\":1,\"Cint1\":13,\"Cstr1\":\"test\",\"Cdate1\":1708339981000},\"source\":{\"version\":\"1.7.1.Final\",\"connector\":\"mysql\",\"name\":\"my-src\",\"ts_ms\":1708339981000,\"snapshot\":\"false\",\"db\":\"test\",\"sequence\":null,\"table\":\"TB_TEST\",\"server_id\":1,\"gtid\":null,\"file\":\"binlog.000003\",\"pos\":5184,\"row\":0,\"thread\":null,\"query\":null},\"op\":\"c\",\"ts_ms\":1708339981565,\"transaction\":null}}";
+        String message = "{\"payload\":{\"before\":null,\"after\":null,\"source\":{\"version\":\"1.7.1.Final\",\"connector\":\"mysql\",\"name\":\"my-src\",\"ts_ms\":1708339981000,\"snapshot\":\"false\",\"db\":\"test\",\"sequence\":null,\"table\":\"TB_TEST\",\"server_id\":1,\"gtid\":null,\"file\":\"binlog.000003\",\"pos\":5184,\"row\":0,\"thread\":null,\"query\":null},\"op\":\"c\",\"ts_ms\":1708339981565,\"transaction\":null}}";
         processService.process(message);
     }
 
